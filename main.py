59ebfd8b4b7a9025ff956e89f2713017caf32a6e
haisul7 <teechanne@proton.me>
2025-08-25 09:10:59 +0300

    index page

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from teapot!")
+import logging
+
+from config import NODE_IDENTITY_PATH, RNS_CONFIGDIR, ANNOUNCE_NAME
+from src.nomadapi import NomadAPI
+from src.nomadapi.app import Config, create_rns_dest
+from src.nomadapi.handlers import Request, render_template
+
+app = NomadAPI(
+    Config(
+        templates_dir='pages'
+    )
+)
+
+@app.request('/page/index.mu')
+def index(r: Request):
+    return render_template('index.mu', dict())
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    dst, identity = create_rns_dest(RNS_CONFIGDIR, NODE_IDENTITY_PATH)
+
+    app.scheduler.every(5).minutes.do(
+        lambda: logging.getLogger("announce").debug(
+            "announce with data %s", ANNOUNCE_NAME
+        )
+        or dst.announce(ANNOUNCE_NAME.encode("utf-8"))
+    )
+
+    app.register_handlers(dst)
+    app.run()
--- src/nomadapi/__init__.py
+++ src/nomadapi/__init__.py
@@ -1,4 +1,3 @@
 import logging
 
 from .app import NomadAPI, create_rns_dest
-from src.config import CONFIG
